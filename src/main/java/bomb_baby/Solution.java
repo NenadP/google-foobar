ab0f6c9a9205458b4d9e9e9384d66122dbb8e2da
NenadP <nenopantic@gmail.com>
2023-01-19 23:38:31 +0000

    Bomb baby solution

--- src/main/java/bomb_baby/Solution.java
+++ src/main/java/bomb_baby/Solution.java
@@ -1,7 +1,46 @@
 package bomb_baby;
 
+import java.math.BigInteger;
+
 public class Solution {
     public static String solution(String x, String y) {
-        return x+y;
+        BigInteger mach = new BigInteger(x);
+        BigInteger facula = new BigInteger(y);
+
+        BigInteger step = BigInteger.ZERO;
+
+        boolean reached = false;
+        boolean impossible = false;
+
+        while (!reached && !impossible) {
+            if (mach.compareTo(facula) > 0) {
+                if (mach.compareTo(facula.multiply(BigInteger.TEN)) > 0) {
+                    BigInteger multi = mach.divide(facula).subtract(BigInteger.ONE);
+                    mach = mach.subtract(facula.multiply(multi));
+                    step = step.add(multi);
+                } else {
+                    mach = mach.subtract(facula);
+                    step = step.add(BigInteger.ONE);
+                }
+            } else {
+                if (facula.compareTo(mach.multiply(BigInteger.TEN)) > 0) {
+                    BigInteger multi = facula.divide(mach).subtract(BigInteger.ONE);
+                    facula = facula.subtract(mach.multiply(multi));
+                    step = step.add(multi);
+                } else {
+                    facula = facula.subtract(mach);
+                    step = step.add(BigInteger.ONE);
+                }
+            }
+
+            reached = mach.compareTo(BigInteger.ONE) == 0 && facula.compareTo(BigInteger.ONE) == 0;
+            impossible = mach.compareTo(BigInteger.ONE) < 0 || facula.compareTo(BigInteger.ONE) < 0;
+        }
+
+        if (reached) {
+            return step.toString();
+        }
+
+        return "impossible";
     }
 }
--- src/test/java/bomb_baby/SolutionTest.java
+++ src/test/java/bomb_baby/SolutionTest.java
@@ -22,4 +22,32 @@ class SolutionTest {
                 Solution.solution("4", "7")
         );
     }
+
+    @Test
+    void test3() {
+        assertEquals(
+                "impossible",
+                Solution.solution("2", "4")
+        );
+    }
+
+    @Test
+    void test4() {
+        // The algo goes in reverse of this example. This is an illustration going from initial available bombs to the target
+        // 1 1 step 0
+        // 1 2 step 1
+        // 1 3 step 2
+        // 4 3 step 3
+        // 7 3 step 4
+        // 7 10 step 5
+        // 17 10 step 6
+        // 17 27 step 7
+        // ... repeating last step of adding 17 for 69 more times to get to:
+        // 17 1200 step 76
+        // or in other words, since mach reached its target, adding 17 to facula happened 70 times
+        assertEquals(
+                "76",
+                Solution.solution("17", "1200")
+        );
+    }
 }
